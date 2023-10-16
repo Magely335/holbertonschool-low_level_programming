904a6183060ae9cec1ef4dc9d5ed054bb7f2c921
Magely335 <7380@holbertonstudents.com>
2023-10-15 22:34:40 -0400

    4.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * 			puzzle, followed by a new line."
  *
  * Return: Always 0.
