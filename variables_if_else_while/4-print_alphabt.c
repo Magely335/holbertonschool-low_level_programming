b8530552e32cd204d3df413f312deb707271302b
Magely335 <7380@holbertonstudents.com>
2023-10-16 15:01:57 -0400

    4.

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		strlen ('q', 'e');
+		putchar (alpha);
+	}
+
+	putchar ('\n');
+	return (0);
+}
