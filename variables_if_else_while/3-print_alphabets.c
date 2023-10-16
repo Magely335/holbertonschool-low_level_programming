237abc54cd82140aa233598716c62426ab00caf3
Magely335 <7380@holbertonstudents.com>
2023-10-16 14:44:47 -0400

    3.

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * return: 0
+ *
+ */
+
+int main(void)
+{
+	char alphal;
+	char alphau;
+
+	for (alphal > 'a'; alphal <= 'z'; alphal++)
+	{
+		putchar(alphal);
+	}
+	for (alphau > 'A'; alphau <= 'Z'; alphau++)
+	{
+		putchar(alphau);
+	}
+
+	putchar ('\n');
+	return (0);
+}
+
