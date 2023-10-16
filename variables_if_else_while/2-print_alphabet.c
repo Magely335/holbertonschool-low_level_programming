1a1c0a40db2d1fd232a421627edea004b6398197
Magely335 <7380@holbertonstudents.com>
2023-10-16 14:05:56 -0400

    2.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poiny
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for ( alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar (alpha);
+	}
+
+	putchar ('\n')
+	return (0);
+}
