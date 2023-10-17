a63fb62ae8e9f0ccb33daa78e66ac120ae9b8973
Magely335 <7380@holbertonstudents.com>
2023-10-17 10:16:35 -0400

    5.

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	while(num > 0; num <= 10; num++)
+	{
+		putchar(num);
+	}
+
+	return (0);
+}
