d8a1ee831f227f418e4ff7e5755b731ba0f10273
Magely335 <7380@holbertonstudents.com>
2023-10-17 13:41:11 -0400

    8.

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: (0)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+	{
+		putchar ('%x', num);
+	}
+	
+	putchar ('\n');
+	return 0;
+}
