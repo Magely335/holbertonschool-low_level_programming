e7104405890c01d8932a16b878b0c347bee988b7
Magely335 <7380@holbertonstudents.com>
2023-10-17 14:27:25 -0400

    8.

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,12 +9,14 @@
 
 int main(void)
 {
-	for (char num = '0'; num <= '9'; num++)
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar (num);
 	}
 
-	for (char num = 'a'; num <= 'f'; num++)
+	for (num = 'a'; num <= 'f'; num++)
 	{
 		putchar (num);
 	}
