b8e3d02817eadd4e0dc5e5cca084bf54fd240d44
Magely335 <7380@holbertonstudents.com>
2023-10-18 11:04:04 -0400

    1.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -11,11 +11,13 @@ void print_alphabet(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'y'; alpha++)
 	{
 		putchar (alpha);
 	}
 
+	putchar ('z');
 	putchar ('\n');
+
 	return (0);
 }
