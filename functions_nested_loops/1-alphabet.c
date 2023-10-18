295b8d2f6bc7f45bd4c2189eaff2c0d0350d15ac
Magely335 <7380@holbertonstudents.com>
2023-10-18 10:16:41 -0400

    1.

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar (alpha);
+	}
+
+	putchar ('\n');
+	return (0);
+}
