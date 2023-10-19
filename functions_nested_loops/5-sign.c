b48ee984a65858bd502b58ada9082f3de1fe1b7e
Magely335 <7380@holbertonstudents.com>
2023-10-19 14:05:29 -0400

    5.

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * @n: checks value
+ * print_sign - Enry point
+ *
+ * Return: 1 if greater than zero, 0 if zero and -1 if less than zero
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	if (n = 0)
+		return (0);
+	else
+		return (-1);
+}
+
