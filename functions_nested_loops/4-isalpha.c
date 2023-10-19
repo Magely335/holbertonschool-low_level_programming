e9d64a34167ff1f62635de2e98217e5ef42d74c8
Magely335 <7380@holbertonstudents.com>
2023-10-19 13:30:46 -0400

    4.

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *@c: value to check
+ *
+ * Return: 1 if character is lowercase or uppercase, 0 if otherwise.
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z'))
+	       return (1);
+	else
+		return (0);
+}	
+
+
+
+		
