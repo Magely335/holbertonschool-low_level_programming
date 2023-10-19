cba749dac72fcb48c73e00a533e1892b3197e397
Magely335 <7380@holbertonstudents.com>
2023-10-18 23:50:01 -0400

    2.

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,13 @@ void print_alphabet_x10(void)
 {
 	char alpha;
 
+	for (int i = 0; i < 10; i++)
+	{
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		putchar (alpha + '0');
 	}
 
 	putchar ('\n');
+	}
 }
