211fe290f2c23bb9bb14e09506efbf86820cd70e
Magely335 <7380@holbertonstudents.com>
2023-10-19 14:41:57 -0400

    5.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,16 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		printf ("+");
-		return (1);
+		return 1;
 	}
 	else if (n == 0)
 	{
 		printf ("0");
-		return (0);
+		return 0;
 	}
 	else
 	{
 		printf ("-");
-		return (-1);
+		return -1;
 	}
 }
