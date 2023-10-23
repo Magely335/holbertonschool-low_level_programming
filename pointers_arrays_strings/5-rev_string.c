db2cc6137f46985b635e34109178e89091e360ae
Magely335 <7380@holbertonstudents.com>
2023-10-23 15:09:03 -0400

    5.

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 	int i, j;
 	char c;
 
-	for (i = 0; j = strlen(s) - 1; i < j; i++, j--)
+	for (i = 0; j = strlen(s) - 1; i < j; i++; j--)
 	{
 		c = s[i];
 		s[i] = s[j];
